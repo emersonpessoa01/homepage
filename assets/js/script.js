2349289c525ddae00e54f2f33675e117c5d2de8c
emersonpessoa01 <eps011108@gmail.com>
2024-08-25 15:40:14 -0300

    feat: Adicionado container com imagens dinâmicas :rocket:

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,41 @@ scrollTop.onclick = () => {
     behavior: "smooth",
   });
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    "./assets/images/certificados/PostgreSQL.png",
+    "./assets/images/carousel/pexels-christina-morillo-1181610.jpg",
+    "./assets/images/carousel/pexels-cottonbro-studio-7437489.jpg",
+    // Adicione mais URLs de imagens conforme necessário
+  ];
+
+  const thumbnailsContainer = document.getElementById("thumbnails");
+  const mainImage = document.getElementById("current-image");
+
+  function removeActiveClass() {
+    const thumbnails = document.querySelectorAll(".thumbnail-carousel img");
+    thumbnails.forEach((thumbnail) => {
+      thumbnail.classList.remove("active");
+    });
+  }
+
+  images.map((image, index) => {
+    const thumbnail = document.createElement("img");
+    thumbnail.src = image;
+    thumbnail.alt = `Imagem ${index + 1}`;
+
+    if (index === 0) {
+      thumbnail.classList.add("active");
+      mainImage.src = image;
+    }
+
+    thumbnail.addEventListener("click", () => {
+      mainImage.src = image;
+      removeActiveClass();
+      thumbnail.classList.add("active");
+    });
+
+    thumbnailsContainer.appendChild(thumbnail);
+  });
+});
